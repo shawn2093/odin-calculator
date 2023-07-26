df742170f1d9efca1f90fd3b421dac1acb43e206
Shawm93 <leeboon1993@gmail.com>
2023-07-27 02:09:28 +0800

    Finish the layout and function of calculator, pending for more action

--- index.js
+++ index.js
@@ -9,108 +9,119 @@ const decimal = document.querySelector('.decimal');
 const equal = document.querySelector('.equal');
 const opers = document.querySelectorAll('.oper');
 
-let number1 = 0;
-let number2 = 0;
-let number2Typed = 0;
-let operand;
-let goodForCalculation = 0;
 let operMode = false;
-let currentNumber = null;
+let canChangeOperand = false;
 let currentOperation = null;
+let equal_number = 0;
+let equal_operand;
 
-// const recording = (input) => {
-//     if (input.target.classList.contains('digit')) {
-//         if (operMode == false){
-//             if (number1 == 0) {
-//                 number1 = input.target.textContent;
-//             } else {
-//                 number1 = number1 + input.target.textContent;
-//             }
-//             display.innerHTML = number1;
-//         } else {
-//             if (number2 == 0) {
-//                 number2 = input.target.textContent;
-//             } else {
-//                 number2 = number2 + input.target.textContent;
-//             }
-//             goodForCalculation = 1;
-//             display.innerHTML = number2;
-//         }
-//     } else if (input.target.classList.contains('oper')) {
+// things to improve:
+// add on keystroke
+// figure out the decimal points console.log(Number(5.1) + Number(0.1))
+// separate the function from the addEventListener
+// add more comments
 
-//         if (operMode == false || goodForCalculation == 0) {
-//             operMode = true;
-//             operand = input.target.textContent;
-//             number1 = parseFloat(number1);
-//             goodForCalculation = 0;
 
-//         } else if (operMode == true && goodForCalculation == 1) {
-//             number2 = parseFloat(number2);
-//             number1 = operator(number1, number2, operand);
-//             operand = input.target.textContent;
-//             number2 = 0;
-//             goodForCalculation = 0;
-//             display.innerHTML = number1;
-//         }    
-//     }
-//     // console.log(number1, number2, operand);
-// }
-
-const operator = (number1, number2, operand) => {
+const operator = (currentOperation, secondNumber) => {
+    let operand = currentOperation.slice(-1);
+    let number1 = Number(currentOperation.slice(0, -1));
+    let number2 = Number(secondNumber);
+    // console.log(number1, number2, operand);
     if (operand == "+") return number1 + number2;
     else if (operand == "-") return number1 - number2;
     else if (operand == "x") return number1 * number2;
     else if (operand == "÷") return number1 / number2;
+    
 }
 
 digits.forEach(digit => digit.addEventListener('click', (e) => {
-    if (display.innerHTML == 0 || currentOperation != null) {
+    if (display.innerHTML == '0') {
         display.innerHTML = e.target.innerHTML;
-        currentOperation = null;
     } else {
-        display.innerHTML += e.target.innerHTML;
+        if (currentOperation != null) {
+            if (canChangeOperand) {
+                display.innerHTML = e.target.innerHTML;
+                canChangeOperand = false;
+            } else {
+                display.innerHTML += e.target.innerHTML;
+            }
+        } else {
+            display.innerHTML += e.target.innerHTML;
+        }
     }
+    equal_number = display.innerHTML;
 }))
 
 opers.forEach(oper => oper.addEventListener('click', (e) => {
     if (currentOperation == null) {
         currentOperation = display.innerHTML + e.target.innerHTML;
-    } else {
+        canChangeOperand = true;
+    } 
+    else {
         // display.innerHTML = evaluate(currentOperation, e.target.innerHTML);
-        console.log(currentOperation);
-        currentOperation = null;
+        if (canChangeOperand) {
+            currentOperation = currentOperation.slice(0, -1) + e.target.innerHTML;
+        } else {
+            display.innerHTML = operator(currentOperation, display.innerHTML);
+            currentOperation = display.innerHTML + e.target.innerHTML;
+            canChangeOperand = true;
+        }
     }
+    equal_number = display.innerHTML;
 }))
 
+equal.addEventListener('click', () => {
+    if (currentOperation != null) {
+        let equal_operand = currentOperation.slice(-1);
+        display.innerHTML = operator(currentOperation, equal_number);
+        currentOperation = display.innerHTML + equal_operand;
+        canChangeOperand = true;
+    }
+})
+
 clear.addEventListener('click', () => {
-    number1 = 0;
-    number2 = 0;
-    number2Typed = 0;
-    operand = undefined;
-    goodForCalculation = 0;
-    operMode = false;
+    currentOperation = null;
+    canChangeOperand = false;
     display.innerHTML = 0;
+    equal_number = 0;
+    equal_operand = null;
 })
 
 opposite.addEventListener('click', () => {
     if (Number(display.innerHTML) != 0) {
         display.innerHTML = Number(display.innerHTML) * (-1);
-        currentNumber = Number(display.innerHTML);
     }
+    equal_number = display.innerHTML;
+    canChangeOperand = false;
+})
+
+percent.addEventListener('click', () => {
+    display.innerHTML = Number(display.innerHTML) / (100);
+    equal_number = display.innerHTML;
+    canChangeOperand = false;
 })
 
 decimal.addEventListener('click', () => {
-    if (display.textContent == '') {
+    if (display.textContent == '' || display.textContent == '-') {
         display.textContent = 0;
     }
-    if (display.textContent.includes('.')) return;
-    display.textContent += '.';
+    if (canChangeOperand) {
+        display.textContent = '0.';
+        canChangeOperand = false;
+    } else {
+        if (display.textContent.includes('.')) return;
+        display.textContent += '.';
+    }
 })
 
 backspace.addEventListener('click', () => {
-    if (display.innerHTML != '') {
+    if (display.innerHTML.length > 1) {
         display.innerHTML = display.innerHTML.slice(0, display.innerHTML.length - 1);
+    } else {
+        display.innerHTML = 0;
     }
+    equal_number = display.innerHTML;
+    canChangeOperand = false;
 })
 
 // btns.forEach(btn => btn.addEventListener('click', recording));
